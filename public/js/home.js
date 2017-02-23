31c4ca092922296e19d3cb2ee0fd705a9ffff213
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-23 13:30:57 -0700

    [ADD]: able to add new grudge

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,9 @@
 const $grudgeContainer = $('.grudge-list-container')
+const $form = $('.js-grudge-form')
+const $nameInput = $('.js-name-input')
+const $descInput = $('.js-desc-input')
+const $dateInput = $('.js-date-input')
+const $addGrudgeBtn = $('.js-add-grudge-btn')
 
 $(document).ready(function() {
   getGrudges()
@@ -14,11 +19,29 @@ const getGrudges = () => {
 
 const displayGrudgeList = (grudges) => {
   //TODO: create document fragment
+  clearGrudges()
   grudges.map(grudge => {
     listHTML(grudge)
   })
 }
 
 const listHTML = (grudge) => {
-  $grudgeContainer.append(`<li class=${grudge.id}><a href="/grudge/${grudge.id}">${grudge.name}</a></li>`)
+  $grudgeContainer.append(`<li class='grudge js-grudge ${grudge.id}'><a href="/grudge/${grudge.id}">${grudge.name}</a></li>`)
+}
+
+$form.on('submit', (e) => {
+  e.preventDefault()
+  const name = $nameInput.val()
+  const offence = $descInput.val()
+  const date = $dateInput.val()
+  addGrudgeToDb(name, offence, date)
+})
+
+const addGrudgeToDb = (name, offence, date) => {
+  axios.post('/api/grudges', {name, offence, date})
+  .then(res => displayGrudgeList(res.data))
+}
+
+const clearGrudges = () => {
+  $('.js-grudge').remove()
 }
