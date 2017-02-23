560c596dc06bfe308da824ec0d1c7ed13c20f9e4
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-23 11:17:45 -0700

    [ADD]: send home file on root path

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
+
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const PORT = process.env.PORT || 1111
+
+app.set('port', PORT)
+app.locals.title = 'GBIN' //change title if necessary
+
+app.locals.grudges = [
+  {
+    id: 1,
+    name: 'Lebron',
+    offense: 'on the cavs',
+    forgiven: false,
+    date: '2/22/17'
+  }
+]
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/views', 'home.html'));
+})
+
+app.get('/api/grudges', (req, res) => {
+  res.status(200).json(app.locals.grudges)
+})
+
+app.listen(PORT, () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`);
+})
